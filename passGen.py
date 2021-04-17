9d8832708c1eae80c1c20b7aba9f42c7d93fad40
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-04-16 21:39:14 -0700

    Update passGen.py
    Password generator inner method

--- passGen.py
+++ passGen.py
@@ -1,8 +1,8 @@
 import random
 import string
 def passwordgen(n):
-
-
-
+    for i in range(n):
+        print(random.choice(string.ascii_letters), end='')
+    return
 number=input("please enter numeric password length")
 passwordgen(int(number))

211dac4593e42f973fc26794e9faa5215f29fd3f
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-04-16 21:34:13 -0700

    driver code
    Added the rest of the driver code to the calculator

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,7 @@ def calculator(num1,num2):
     subtract = num1-num2
     multiply = num1*num2
     divide = num1/num2
+    print("sum:", add, " difference:", subtract, " product:", multiply, " quotient:", int(divide))
+number1 = input("please enter first number")
+number2 = input("please enter second number")
+calculator(int(number1),int(number2))
